3ae13d92a982f02d9d70db87995a0f6fbb0d1525
libmartinito <lib.martinito@pm.me>
2023-09-18 16:37:24 +0800

    CC-728 added compiled files

---
+++ solutions/cpp/01-init/code/src/Server.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+bool match_pattern(const std::string& input_line, const std::string& pattern) {
+    if (pattern.length() == 1) {
+        return input_line.find(pattern) != std::string::npos;
+    }
+    else {
+        throw std::runtime_error("Unhandled pattern " + pattern);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    // std::cout << "Number of arguments: " << argc << "\n";
+    // for (int i = 0; i < argc; ++i) {
+    //   std::cout << "argv[" << i << "] = " << argv[i] << "\n";
+    // }
+
+    if (argc != 3) {
+        std::cerr << "Expected two arguments" << std::endl;
+        return 1;
+    }
+
+    std::string flag = argv[1];
+    std::string pattern = argv[2];
+
+    if (flag != "-E") {
+        std::cerr << "Expected first argument to be '-E'" << std::endl;
+        return 1;
+    }
+
+
+    std::string input_line;
+    std::getline(std::cin, input_line);
+
+    try {
+        if (match_pattern(input_line, pattern)) {
+            return 0;
+        } else {
+            return 1;
+        }
+    } catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+}
