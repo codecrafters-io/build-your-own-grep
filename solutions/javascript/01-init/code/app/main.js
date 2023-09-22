df5ff7e50fdbc0df82209f6a226bce0d954af579
libmartinito <lib.martinito@pm.me>
2023-09-22 15:41:20 +0800

    CC-754 added compiled files

---
+++ solutions/javascript/01-init/code/app/main.js
@@ -0,0 +1,25 @@
+function matchPattern(inputLine, pattern) {
+  if (pattern.length === 1) {
+    return inputLine.includes(pattern);
+  } else {
+    throw new Error(`Unhandled pattern ${pattern}`);
+  }
+}
+
+function main() {
+  const pattern = process.argv[3];
+  const inputLine = require("fs").readFileSync(0, "utf-8").trim();
+
+  if (process.argv[2] !== "-E") {
+    console.log("Expected first argument to be '-E'");
+    process.exit(1);
+  }
+
+  if (matchPattern(inputLine, pattern)) {
+    process.exit(0);
+  } else {
+    process.exit(1);
+  }
+}
+
+main();
