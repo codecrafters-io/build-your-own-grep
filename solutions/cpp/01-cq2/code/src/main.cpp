81526ad04766261bba9e282ffc8fbf07972377d9
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-12-10 07:50:47 +0900

    Update documentation for multiple matches processing and clarify instructions

--- solutions/cpp/01-cq2/code/src/main.cpp
+++ solutions/cpp/01-cq2/code/src/main.cpp
@@ -4,8 +4,7 @@
 bool match_pattern(const std::string& input_line, const std::string& pattern) {
     if (pattern.length() == 1) {
         return input_line.find(pattern) != std::string::npos;
-    }
-    else {
+    } else {
         throw std::runtime_error("Unhandled pattern " + pattern);
     }
 }
